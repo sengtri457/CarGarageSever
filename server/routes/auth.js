3ddcb6af13154ad4178aee44299c709281166383
Sengtri Bun <sengtri457@gmail.com>
2025-12-31 15:21:58 +0700

    add

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+const User = require("../models/User");
 
 // --- Register ---
 router.post("/register", async (req, res) => {
